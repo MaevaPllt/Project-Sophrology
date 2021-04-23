61e638871baf9f82ebf5c0b645eb0b8550d26b11
MaevaPllt <maeva.pllt@gmail.com>
2021-04-23 14:24:27 +0200

    fix bootstrap js

--- assets/app.js
+++ assets/app.js
@@ -8,31 +8,35 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 import './styles/navbar.scss';
+import bsCustomFileInput from 'bs-custom-file-input';
+
 
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
+
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
-});
+    bsCustomFileInput.init();
 
+    const eye = document.querySelector(".eye");
+    const eyeoff = document.querySelector(".eye-off");
+    const passwordField = document.querySelector("input[type=password]");
 
-const eye = document.querySelector(".eye");
-const eyeoff = document.querySelector(".eye-off");
-const passwordField = document.querySelector("input[type=password]");
+    eye.addEventListener("click", () => {
+        eye.style.display = "none";
+        eyeoff.style.display = "block";
+        passwordField.type = "text";
+    });
 
-eye.addEventListener("click", () => {
-    eye.style.display = "none";
-    eyeoff.style.display = "block";
-    passwordField.type = "text";
-});
+    eyeoff.addEventListener("click", () => {
+        eyeoff.style.display = "none";
+        eye.style.display = "block";
+        passwordField.type = "password";
+    });
 
-eyeoff.addEventListener("click", () => {
-    eyeoff.style.display = "none";
-    eye.style.display = "block";
-    passwordField.type = "password";
 });
