093bdb921969db83b40d94c2157443c230a2f709
MaevaPllt <maeva.pllt@gmail.com>
2021-04-23 12:18:54 +0200

    added eye password

--- assets/app.js
+++ assets/app.js
@@ -19,3 +19,20 @@ require('bootstrap');
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
 });
+
+
+const eye = document.querySelector(".eye");
+const eyeoff = document.querySelector(".eye-off");
+const passwordField = document.querySelector("input[type=password]");
+
+eye.addEventListener("click", () => {
+    eye.style.display = "none";
+    eyeoff.style.display = "block";
+    passwordField.type = "text";
+});
+
+eyeoff.addEventListener("click", () => {
+    eyeoff.style.display = "none";
+    eye.style.display = "block";
+    passwordField.type = "password";
+});
