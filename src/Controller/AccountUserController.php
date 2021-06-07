eacd17598b5715263b92eaf47de457bd4d299480
MaevaPllt <maeva.pllt@gmail.com>
2021-06-07 20:46:07 +0200

    add delete account user

--- src/Controller/AccountUserController.php
+++ src/Controller/AccountUserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
- * Class AdminHomeController
- * @package App\Controller
  * @IsGranted("ROLE_USER")
  * @Route("/membre", name="user_")
  */
@@ -37,5 +36,49 @@ class AccountUserController extends AbstractController
         }
 
         return $this->render('account_user/user_home.html.twig');
+    }
+
+    /**
+     * @Route("/{id}/edit", name="edit", methods={"GET","POST"})
+     * @param Request $request
+     * @param User $user
+     * @return Response
+     */
+    public function edit(Request $request, User $user): Response
+    {
+
+        $form = $this->createForm(UserType::class, $this->getUser());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('success', 'Vos modifications ont bien été mises à jour !');
+        }
+
+        return $this->render('account_user/edit.html.twig', [
+            'user' => $user,
+            'formEditUser' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="delete", methods={"DELETE"})
+     * @param Request $request
+     * @param User $user
+     * @return Response
+     */
+    public function delete(Request $request, User $user): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
+            $this->container->get('security.token_storage')->setToken(null);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre compte a bien été supprimé !');
+        }
+
+        return $this->redirectToRoute('home');
     }
 }
